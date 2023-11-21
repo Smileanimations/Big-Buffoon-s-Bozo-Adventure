6a0e53b5361431125342d4bdb1421dedcb05a1ed
ZakrinYoran <145646699+ZakrinYoran@users.noreply.github.com>
2023-11-21 06:06:06 -0900

    oops my finger slipped
    more character functionality + cleanup mark code

--- Most Ambitious shit ive done!/scripts/charformat.gd
+++ Most Ambitious shit ive done!/scripts/charformat.gd
@@ -1,49 +1,74 @@
 extends Node
 
-@export var statsbase = {
-	"Health" : 100,
+@export var statsBase = {
+	"HealthMax" : 100,
 	"Speed" : 5,
 	"Defense" : 20,
 }
 
 @export var statsPerLevel = {
-	"Health" : 5,
+	"HealthMax" : 5,
 	"Speed" : 1,
 	"Defense" : 2,
 }
 
 @export var stats = {
 	"XP" : 0,
-	"Level" : 0,
 }
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	calcStats()
+	setHealth(INF)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+#	pass
+
+
+func setHealth(val):
+	stats["Health"] = clamp(val, 0, stats["HealthMax"])
 
 
 func calcStats():
-	for stat in statsbase:
-		stats[stat] = statsbase[stat] + (statsPerLevel[stat] * stats["Level"])
-		print(stats)
+	calcLevel()
+	for stat in statsBase:
+		stats[stat] = statsBase[stat] + (statsPerLevel[stat] * stats["Level"])
+	print(stats)
+
+var xpToNext = 0
+func calcLevel():
+	var xp = stats["XP"]
+	var Lvl = 0
+	var nxtLvl = 100
+	var lvlScale = 1.3
+	while xp >= nxtLvl:
+		xp -=nxtLvl
+		nxtLvl = int(nxtLvl * lvlScale)
+		Lvl += 1
+	xpToNext = nxtLvl - xp
+	stats["Level"] = Lvl
+
 
 func levelUp():
-	stats["Level"] += 1
+	calcLevel()
+	print("Adding %s XP to level up." % xpToNext)
+	stats["XP"] += xpToNext
 	calcStats()
 
+
 func applyDamage(damage):
-	stats["Health"] -= max(damage - stats["Defense"], 0)
+	print("Applying %s damage to %s" % [damage, name])
+	damage = max(damage - stats["Defense"], 0)
+	print("Damage reduced to %s by %s defense" % [damage, stats["Defense"]])
+	stats["Health"] = max(stats["Health"] - damage, 0)
 	print(stats)
 
 
-func _on_pressed():
-	pass # Replace with function body.
+func applyHealing(healing):
+	print("Applying %s healing to %s" % [healing, name])
+	stats["Health"] = min(stats["Health"] + healing, stats["HealthMax"])
+	print(stats)
 
-
-func _on_button_2_pressed():
-	pass # Replace with function body.
