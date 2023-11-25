7f0d4717a7226d46961c67448282c9c0e76afcdc
ZakrinYoran <145646699+ZakrinYoran@users.noreply.github.com>
2023-11-25 02:48:00 -0900

    saucer

---
+++ moves/template/moveScript.gd
@@ -0,0 +1,39 @@
+extends Node
+
+# Signal is emitted when the move is selected
+signal wasSelected
+
+# What character the move belongs to
+var character
+
+@onready var selectButton = $MoveSelect
+@onready var nameLabel = $MoveLabels/MoveName
+@onready var descLabel = $MoveLabels/MoveDesc
+
+# The name and description of the move
+@export var moveName : String
+@export var moveDesc : String
+
+# A dictionary to add move properties, such as damage, healing, bleed or poison effects
+@export var moveEffects : Dictionary
+# Who the move can target
+@export_enum("Self", "Allies", "Enemies", "All") var moveTargets : String = "All"
+
+
+# Connects the move to the character that owns it
+func startup(c):
+	character = c
+	selectButton.pressed.connect(moveSelected)
+	wasSelected.connect(character.moveSelected)
+
+
+# Set up labels and node name
+func _ready():
+	set_name.call_deferred(moveName)
+	nameLabel.text = moveName
+	descLabel.text = moveDesc
+
+
+# Runs when the move is selected
+func moveSelected():
+	wasSelected.emit(self)
