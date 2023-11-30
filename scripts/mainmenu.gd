78ad73ac7af70913cdd550a7b11e18459e3d29cb
Mark <iksnij12@gmail.com>
2023-11-30 12:37:58 +0100

    I added MUSIC ZAKRIN!

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,14 +1,27 @@
 extends Node
 
+@onready var music = $Audio
+
 func _ready():
-	pass
+	music.play()
+	
 func _process(delta):
 	pass
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/FightTest.tscn")
-
+	music.stream_paused = true
+	
 func _on_quit_pressed():
 	get_tree().quit()
 	
-func _on_Option_pressed():
-	get_tree().quit()
+func _on_option_pressed():
+	get_tree().change_scene_to_file("res://scenes/Options.tscn")
+
+
+
+
+#Options
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main Menu.tscn")
+	
+
